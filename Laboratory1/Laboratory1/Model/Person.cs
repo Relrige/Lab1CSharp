48f7e9c9b5621399698ede0ab852550987213d41
Relrige <91017454+Relrige@users.noreply.github.com>
2025-03-22 14:34:50 +0200

    add lab1 solution

---
+++ Laboratory1/Laboratory1/Model/Person.cs
@@ -0,0 +1,48 @@
+﻿using Laboratory1.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratory1.Model
+{
+    public class Person
+    {
+        #region Fields
+
+        private int _age;
+        private DateTime _dob = DateTime.Today;
+        private ZodiacSigns _zodiac;
+        private ChineseZodiacSigns _chineseZodiac;
+
+        #endregion
+
+        #region Properties
+        public int Age
+        {
+            get => _age;
+            set => _age = value;
+        }
+        public DateTime DateOfBirth
+        {
+            get => _dob;
+            set => _dob = value;
+        }
+        public ZodiacSigns WesternZodiac
+        {
+            get => _zodiac;
+            set => _zodiac = value;
+        }
+        public ChineseZodiacSigns ChineseZodiac
+        {
+            get => _chineseZodiac;
+            set => _chineseZodiac = value;
+        }
+        #endregion
+        public bool IsBirthdayToday()
+        {
+            return DateOfBirth.Day == DateTime.Today.Day && DateOfBirth.Month == DateTime.Today.Month;
+        }
+    }
+}
---
+++ Laboratory1/Laboratory1/ViewModel/MainViewModel.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using CommunityToolkit.Mvvm.Input;
+using Laboratory1.Helper;
+using Laboratory1.Model;
+
+namespace Laboratory1.ViewModel
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        #region fieldsAndProperties
+        private Person _person = new Person { DateOfBirth = DateTime.Today };
+        private RelayCommand<object> _relayComand;
+
+        public int Age
+        {
+            get => _person.Age;
+            private set
+            {
+                _person.Age = value;
+                OnPropertyChanged();
+            }
+        }
+        public DateTime DateOfBirth
+        {
+            get => _person.DateOfBirth;
+            set
+            {
+                if (_person.DateOfBirth != value)
+                {
+                    _person.DateOfBirth = value;
+                    //DateChanged();
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public ZodiacSigns WesternZodiac
+        {
+            get => _person.WesternZodiac;
+            private set
+            {
+                _person.WesternZodiac = value;
+                OnPropertyChanged();
+            }
+        }
+        public ChineseZodiacSigns ChineseZodiac
+        {
+            get => _person.ChineseZodiac;
+            private set
+            {
+                _person.ChineseZodiac = value;
+                OnPropertyChanged();
+            }
+        }
+        #endregion
+
+        #region calculate
+        private void DateChanged()
+        {
+            Age = DateHelper.CalculateAge(DateOfBirth);
+            if (!ValidateAge(Age))
+            {
+                MessageBox.Show("Invalid age. Please enter a valid date of birth.");
+                return;
+            }
+            if (_person.IsBirthdayToday())
+                MessageBox.Show("Happy birthday!");
+            WesternZodiac = DateHelper.GetWesternZodiac(DateOfBirth);
+            ChineseZodiac = DateHelper.GetChineseZodiac(DateOfBirth);
+        }
+        private bool ValidateAge(int age)
+        {
+            return age is >= 0 and < 135;
+        }
+        #endregion
+
+        public RelayCommand<object> CalculateCommand =>
+          _relayComand ??= new RelayCommand<object>(_ => DateChanged());
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
